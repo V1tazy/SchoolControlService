439d02d07ee21aec2efda7b0d90c4de2d1230892
V1tazy <132578253+V1tazy@users.noreply.github.com>
2025-01-15 09:00:26 +0900

    Реализация контейнера сервисов

--- AirlineControlService/App.xaml.cs
+++ AirlineControlService/App.xaml.cs
@@ -1,14 +1,35 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
 
 namespace AirlineControlService
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private static IHost _Host;
+
+        public static IHost Host => _Host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => _Host.Services;
+
+        internal static void ConfigureService(HostBuilderContext host, IServiceCollection services)
+        {
+
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Host;
+            base.OnStartup(e);
+            await host.StartAsync();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using var host = Host;
+            base.OnExit(e);
+            await host.StopAsync();
+        }
     }
 
 }
---
+++ AirlineControlService/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace AirlineControlService
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(App.ConfigureService);
+    }
+}
