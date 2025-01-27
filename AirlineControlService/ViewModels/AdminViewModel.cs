6b45570b2671cd5220c4c46ef7fb872556730ca6
V1tazy <132578253+V1tazy@users.noreply.github.com>
2025-01-27 20:42:18 +0900

    базовая верстка админ + рабочие комманды в vm

--- AirlineControlService/ViewModels/AdminViewModel.cs
+++ AirlineControlService/ViewModels/AdminViewModel.cs
@@ -1,13 +1,46 @@
-﻿using AirlineControlService.VIewModels.Base;
+﻿using AirlineControlService.Infrastructure.Commands;
+using AirlineControlService.VIewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace AirlineControlService.ViewModels
 {
     class AdminViewModel: ViewModel
     {
+        #region Command
+
+        public ICommand ScheduleSelectCommand { get; }
+        public ICommand ChildSelectCommand { get; }
+        public ICommand UserSelectCommand { get; }
+
+        private async void OnScheduleSelectCommand(object p) 
+        {
+
+        }
+
+        private async void OnChildSelectCommand(object p) 
+        {
+            
+        }
+
+        private async void OnUserSelectCommand(object p) 
+        {
+
+        }
+
+        #endregion
+
+
+
+        public AdminViewModel() 
+        {
+            ScheduleSelectCommand = new LambdaCommand(OnScheduleSelectCommand);
+            ChildSelectCommand = new LambdaCommand(OnChildSelectCommand);
+            UserSelectCommand = new LambdaCommand(OnUserSelectCommand);
+        }
     }
 }
